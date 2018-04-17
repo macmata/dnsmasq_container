f7c7361c02c44fdba2afae7a7926ef83ca9c2ab8
Alexandre Leblanc <alexandre.leblanc@savoirfairelinux.com>
2018-04-17 16:12:23 -0400

    docker_c: rename script
    change dnsmaq config

---
+++ set_filewall.sh
@@ -0,0 +1,4 @@
+firewall-cmd  --permanent --add-port=67-69/udp
+firewall-cmd  --permanent --add-port=67-69/tcp
+firewall-cmd  --permanent --add-port=6000/tcp
+firewall-cmd  --permanent --add-port=6500/tcp
